a7e3e9fd3ce7c7aa00767b9b839422088db73934
Rafaela Garcia <C0648322@vale.com>
2025-04-12 00:53:55 -0300

    🎻 - Correção estrutura

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class ContaTerminal {
-    public static void main(String[] args) {
+    public static void concat() {
 
         String mensagem;
         Scanner inputScanner = new Scanner(System.in);
